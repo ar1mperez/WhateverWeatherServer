955e108da0635bf9a2a0764ee7487179e2dba290
Ari <aperez02@uoguelph.ca>
2019-01-27 02:46:46 -0500

    Worked on code to dynamically load images

--- public/index.js
+++ public/index.js
@@ -22,15 +22,52 @@ $( document ).ready(function() {
     initialText.innerHTML += data.temp + "°C in " + data.city;
 
 
+    let i = 0;
+
+    //for (i = 0; i < 3 i ++) {
+
+        let div = document.getElementById('clothes-container');
+        let imgContainer = document.createElement('div');
+        imgContainer.classList.add('weather-box');
+
+        let newImage = document.createElement("img");
+        newImage.src = "images/1.png";//+data.tops[i];
+        newImage.classList.add("rounded");
+        newImage.classList.add("mx-auto");
+        newImage.classList.add("d-block");
+        newImage.classList.add("embed-responsive-16by9");
+        newImage.classList.add("clothing-image");
+        imgContainer.append(newImage);
+        div.append(imgContainer);
+    //}
     /*
-    let div = document.getElementById('clothes-container');
-    let imgContainer = document.createElement('div');
     imgContainer.innerHTML +=
-    '<div id = "formdiv" style = "" class="container">\
-    <img src = "images/jacket-alt.png" class="rounded mx-auto d-block embed-responsive-16by9 clothing-image"/>\
-    </div>\
+    '<div class="weather-box">\
+    <img src = "images/1.png" class="rounded mx-auto d-block embed-responsive-16by9 clothing-image"/></div>\
+    <div class="weather-box">\
+    <img src = "images/3.png" class="rounded mx-auto d-block embed-responsive-16by9 clothing-image"/></div>\
     ';
-    div.append(imgContainer);
     */
+    div.append(imgContainer);
+    /*
+    let i = 0;
+    for (i = 0; i < 3; i++) {
+      if (data.tops[i] != null) {
+        let div = document.getElementById('clothes-container');
+        let imgContainer = document.createElement('div');
+        imgContainer.innerHTML +=
+        '<div id = "formdiv" style = "" class="container">\
+        <img src = "images/" class="rounded mx-auto d-block embed-responsive-16by9 clothing-image"/>\
+        </div>\
+        ';
+        div.append(imgContainer);
+
+      }
+
+    }
+
+    */
+
+
   });
 });
